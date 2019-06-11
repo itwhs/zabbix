7219ab12bd612fbda12367922699b34df6672b24
itwhs <42338078+itwhs@users.noreply.github.com>
2019-06-11 18:00:18 +0800

    Update zabbix.sh

--- zabbix/zabbix.sh
+++ zabbix/zabbix.sh
@@ -125,9 +125,12 @@ sed -ri /^http/a"\ \ \ \ include\ \ \ \ \ \ \ vhost.type;" $nginxconf
 cat >> $nginxvhost <<'WHS'
     server {
         listen       80;
-		index index.php index.html;
+	index index.php index.html;
         server_name  zabbix.wenhs.com;
-        access_log  logs/zabbix.log;
+        log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+        access_log  logs/zbxaccess.log main;
         location ~ \.php$ {
             root           html/zabbix;
             fastcgi_pass   127.0.0.1:9000;
